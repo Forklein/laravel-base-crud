3cff669c6787505ea76eca426dd088208de5e526
Forklein <giuseppepisani2@gmail.com>
2021-10-11 18:02:46 +0200

    Add card Details

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -45,8 +45,9 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Comic $id)
+    public function show($id)
     {
+        $comic = Comic::findorfail($id);
         return view('comics.show', compact('comic'));
     }
 
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,1 +1,14 @@
-<h1>Test</h1>
+@extends('layouts.main')
+
+@section('title', $comic->title)
+
+@section('content')
+    <div class="container">
+        <div class="card mx-auto mt-3 p-2" style="width: 18rem;">
+            <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{$comic->title}}">
+            <div class="card-body">
+              <p class="card-text">{{$comic->description}}</p>
+            </div>
+        </div>
+    </div>
+@endsection
