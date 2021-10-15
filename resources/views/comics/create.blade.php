774a928c70e3e99a0c6abad818cb31ab5807a829
Forklein <giuseppepisani2@gmail.com>
2021-10-15 17:36:52 +0200

    add message for error in field with @error

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -21,8 +21,14 @@
             <div class="col-6">
                 <div class="mb-3">
                     <label for="title" class="form-label">Title</label>
-                    <input type="text" class="form-control" id="title" name="title" value="{{old('title')}}">
+                    <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old('title')}}">
+                    @error('title')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                    @else
                     <div class="form-text">Inserisci il titolo</div>
+                    @enderror
                   </div>
             </div>
             <div class="col-6">
@@ -42,29 +48,53 @@
             <div class="col-6">
                 <div class="mb-3">
                     <label for="price" class="form-label">Price</label>
-                    <input type="text" class="form-control" id="price" name="price" placeholder="2.99" value="{{old('price')}}">
+                    <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price" placeholder="2.99" value="{{old('price')}}">
+                    @error('price')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                    @else
                     <div class="form-text">Inserisci il prezzo</div>
+                    @enderror
                 </div>
             </div>
             <div class="col-6">
                 <div class="mb-3">
                     <label for="series" class="form-label">Series</label>
-                    <input type="text" class="form-control" id="series" name="series" value="{{old('series')}}">
+                    <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" value="{{old('series')}}">                  
+                    @error('series')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                    @else
                     <div class="form-text">Inserire la serie</div>
+                    @enderror
                 </div>
             </div>
             <div class="col-6">
                 <div class="mb-3">
                     <label for="sale_date" class="form-label">Sale Date</label>
-                    <input type="text" class="form-control" id="sale_date" name="sale_date" placeholder="2020-08-09" value="{{old('sale_date')}}">
+                    <input type="text" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date" placeholder="2020-08-09" value="{{old('sale_date')}}">
+                    @error('sale_date')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                    @else
                     <div class="form-text">Inserisci la data di vendita</div>
+                    @enderror
                 </div>
             </div>
             <div class="col-6 mx-auto">
                 <div class="mb-3">
                     <label for="type" class="form-label">Type</label>
-                    <input type="text" class="form-control" id="type" name="type" value="{{old('type')}}">
+                    <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name="type" value="{{old('type')}}">
+                    @error('type')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                    @else
                     <div class="form-text">Inserisci il tipo</div>
+                    @enderror
                 </div>
             </div>
             <div class="btn mx-auto">
